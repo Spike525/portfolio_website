07d0d31e3473c6a6d9c59a080ac6ab33edcb2549
Spike525 <sspike1.1.1@gmail.com>
2020-09-15 11:01:55 +0300

    Section B of index page

--- js/typewriter.js
+++ js/typewriter.js
@@ -60,7 +60,6 @@ function init() {
   const txtElement = document.querySelector('.txt-type');
   const words = JSON.parse(txtElement.getAttribute('data-words'));
   const wait = txtElement.getAttribute('data-wait');
-  console.log(words);
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
